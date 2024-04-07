500dc149b811fc4f38698b3eb0820f0f6dae77a9
1ndev-ui <1ndevelopment@protonmail.com>
2024-04-06 23:32:25 -0700

    Adjusted for ArrowOS 13.1

--- arrow_CG65.mk
+++ arrow_CG65.mk
@@ -9,13 +9,13 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 
 # Inherit some common Lineage stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+$(call inherit-product, vendor/arrow/config/common.mk)
 
 # Inherit from CG65 device
 $(call inherit-product, device/vortex/CG65/device.mk)
 
 PRODUCT_DEVICE := CG65
-PRODUCT_NAME := lineage_CG65
+PRODUCT_NAME := arrow_CG65
 PRODUCT_BRAND := Vortex
 PRODUCT_MODEL := CG65
 PRODUCT_MANUFACTURER := vortex
